87f069d776850d4a8d5c61ba18432331c9320039
Luis Rivera Gonzalez <riveragluisd@gmail.com>
2025-02-06 10:11:21 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_RiveraGonzalezLuis.py
@@ -0,0 +1,8 @@
+# Luis Rivera Gonzalez
+# 2/4/2025
+# P1LAB1
+# get input from the user and display output
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
