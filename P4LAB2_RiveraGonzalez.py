c175bb7347335b065846a6bb102fbda7159d71a5
Luis Rivera Gonzalez <riveragluisd@gmail.com>
2025-05-13 11:54:47 -0400

    P4LAB2

---
+++ P4LAB2_RiveraGonzalez.py
@@ -0,0 +1,20 @@
+# Luis Rivera Gonzalez
+# 3/20/2025
+# P4LAB1
+# Displays information to users using loops.
+
+# Variable to control while loop
+run_again = "yes"
+
+# While loop to control the main logic
+while run_again.lower() != "no":
+    integer = int(input("Enter an integer: "))
+    if integer >= 0:
+        for i in range(1,13):
+            print(f"{integer} * {i} = {integer * i}")
+    else:
+        print("This program does not handle negative number")
+    run_again = input("Would you like to run the program again? ")
+
+# While Loop ends here
+print("Okay, Goodbye!")
